67e51ea6f1f105e536c91136ee80a46912ebc62f
josephchoe <joseph.choe@jibjab.com>
2018-02-12 13:03:50 -0800

    Add import rake task and ImportCensus class

---
+++ population-growth-api/app/services/etl/import_census.rb
@@ -0,0 +1,46 @@
+module ETL
+  class ImportCensus
+    def initialize(zip_to_cbsa:, cbsa_to_msa:, msa:)
+      self.zip_to_cbsa = zip_to_cbsa
+      self.cbsa_to_msa = cbsa_to_msa
+      self.msa = msa
+    end
+
+    def import
+      zip_to_cbsa.each do |zip, cbsa_array|
+        zip_code = ZipCode.find_or_create_by(zip_code: zip)
+
+        cbsa_array.each do |cbsa|
+          alternate_cbsa = cbsa_to_msa[cbsa.to_sym]
+          true_cbsa = alternate_cbsa.nil? ? cbsa : alternate_cbsa.first
+
+          cbsa_record = CoreBasedStatisticalArea.find_or_create_by(cbsa: true_cbsa)
+          ZipCodeCoreBasedStatisticalArea.find_or_create_by(
+            zip_code: zip_code,
+            core_based_statistical_area: cbsa_record
+          )
+          metro_row = msa[true_cbsa.to_sym]
+          next if metro_row.nil?
+          name = metro_row[:name]
+          population_2014 = metro_row[:population_2014]
+          population_2015 = metro_row[:population_2015]
+          metro_area = MetropolitanStatisticalArea.find_or_create_by(
+            name: name,
+            population_2014: population_2014,
+            population_2015: population_2015
+          )
+          CoreBasedStatisticalAreaMetropolitanStatisticalArea.find_or_create_by(
+            core_based_statistical_area: cbsa_record,
+            metropolitan_statistical_area: metro_area
+          )
+        end
+      end
+    end
+
+    private
+
+    attr_accessor :zip_to_cbsa,
+                  :cbsa_to_msa,
+                  :msa
+  end
+end
---
+++ population-growth-api/spec/services/etl/import_census_spec.rb
@@ -0,0 +1,268 @@
+require 'rails_helper'
+
+RSpec.describe ETL::ImportCensus do
+  describe '#import' do
+    it 'creates zip code' do
+      service = build_normal_service
+      expect { service.import }
+        .to change(
+          ZipCode.where(zip_code: '10000'), :count
+        ).by(1)
+    end
+
+    it 'creates core based statistical area' do
+      service = build_normal_service
+      expect { service.import }
+        .to change(
+          CoreBasedStatisticalArea.where(cbsa: '20000'), :count
+        ).by(1)
+    end
+
+    it 'creates metropolitan statistical area' do
+      service = build_normal_service
+      expect { service.import }
+        .to change(
+          MetropolitanStatisticalArea.where(name: 'Test'), :count
+        ).by(1)
+    end
+
+    it 'creates zip code mapped to core based statistical area' do
+      service = build_normal_service
+      zip_code = create(:zip_code)
+      cbsa = create(:core_based_statistical_area)
+      expect { service.import }
+        .to change(
+          ZipCodeCoreBasedStatisticalArea.where(
+            zip_code_id: zip_code.id,
+            core_based_statistical_area_id: cbsa.id
+          ), :count
+        ).by(1)
+    end
+
+    it 'creates core based statistical area mapped to metropolitan statistical area' do
+      service = build_normal_service
+      cbsa = create(:core_based_statistical_area)
+      msa = create(:metropolitan_statistical_area)
+      expect { service.import }
+        .to change(
+          CoreBasedStatisticalAreaMetropolitanStatisticalArea.where(
+            core_based_statistical_area_id: cbsa.id,
+            metropolitan_statistical_area: msa.id
+          ), :count
+        ).by(1)
+    end
+
+    context 'when zip code has no cbsa' do
+      it 'creates zip code' do
+        service = build_no_cbsa_service
+        expect { service.import }
+          .to change(
+            ZipCode.where(zip_code: '10000'), :count
+          ).by(1)
+      end
+
+      it 'creates core based statistical area' do
+        service = build_no_cbsa_service
+        expect { service.import }
+          .to change(
+            CoreBasedStatisticalArea.where(cbsa: '20000'), :count
+          ).by(1)
+      end
+
+      it 'does not create metropolitan statistical area' do
+        service = build_no_cbsa_service
+        expect { service.import }
+          .not_to change(
+            MetropolitanStatisticalArea.where(name: 'Test'), :count
+          )
+      end
+
+      it 'creates zip code mapped to core based statistical area' do
+        service = build_no_cbsa_service
+        zip_code = create(:zip_code)
+        cbsa = create(:core_based_statistical_area)
+        expect { service.import }
+          .to change(
+            ZipCodeCoreBasedStatisticalArea.where(
+              zip_code_id: zip_code.id,
+              core_based_statistical_area_id: cbsa.id
+            ), :count
+          ).by(1)
+      end
+
+      it 'does not create core based statistical area mapped to metropolitan statistical area' do
+        service = build_no_cbsa_service
+        cbsa = create(:core_based_statistical_area)
+        expect { service.import }
+          .not_to change(
+            CoreBasedStatisticalAreaMetropolitanStatisticalArea.where(
+              core_based_statistical_area_id: cbsa.id
+            ), :count
+          )
+      end
+    end
+
+    context 'when cbsa has no msa' do
+      it 'creates zip code' do
+        service = build_no_msa_service
+        expect { service.import }
+          .to change(
+            ZipCode.where(zip_code: '10000'), :count
+          ).by(1)
+      end
+
+      it 'creates core based statistical area' do
+        service = build_no_msa_service
+        expect { service.import }
+          .to change(
+            CoreBasedStatisticalArea.where(cbsa: '20000'), :count
+          ).by(1)
+      end
+
+      it 'does not create metropolitan statistical area' do
+        service = build_no_msa_service
+        expect { service.import }
+          .not_to change(
+            MetropolitanStatisticalArea.where(name: 'Test'), :count
+          )
+      end
+
+      it 'creates zip code mapped to core based statistical area' do
+        service = build_no_msa_service
+        zip_code = create(:zip_code)
+        cbsa = create(:core_based_statistical_area)
+        expect { service.import }
+          .to change(
+            ZipCodeCoreBasedStatisticalArea.where(
+              zip_code_id: zip_code.id,
+              core_based_statistical_area_id: cbsa.id
+            ), :count
+          ).by(1)
+      end
+
+      it 'does not create core based statistical area mapped to metropolitan statistical area' do
+        service = build_no_msa_service
+        cbsa = create(:core_based_statistical_area)
+        expect { service.import }
+          .not_to change(
+            CoreBasedStatisticalAreaMetropolitanStatisticalArea.where(
+              core_based_statistical_area_id: cbsa.id
+            ), :count
+          )
+      end
+    end
+
+    context 'when has alternate cbsa' do
+      it 'creates zip code' do
+        service = build_alternate_cbsa_service
+        expect { service.import }
+          .to change(
+            ZipCode.where(zip_code: '10000'), :count
+          ).by(1)
+      end
+
+      it 'creates core based statistical area' do
+        service = build_alternate_cbsa_service
+        expect { service.import }
+          .to change(
+            CoreBasedStatisticalArea.where(cbsa: '20000'), :count
+          ).by(1)
+      end
+
+      it 'creates metropolitan statistical area' do
+        service = build_alternate_cbsa_service
+        expect { service.import }
+          .to change(
+            MetropolitanStatisticalArea.where(name: 'Test'), :count
+          ).by(1)
+      end
+
+      it 'creates zip code mapped to core based statistical area' do
+        service = build_alternate_cbsa_service
+        zip_code = create(:zip_code)
+        cbsa = create(:core_based_statistical_area)
+        expect { service.import }
+          .to change(
+            ZipCodeCoreBasedStatisticalArea.where(
+              zip_code_id: zip_code.id,
+              core_based_statistical_area_id: cbsa.id
+            ), :count
+          ).by(1)
+      end
+
+      it 'creates core based statistical area mapped to metropolitan statistical area' do
+        service = build_alternate_cbsa_service
+        cbsa = create(:core_based_statistical_area)
+        msa = create(:metropolitan_statistical_area)
+        expect { service.import }
+          .to change(
+            CoreBasedStatisticalAreaMetropolitanStatisticalArea.where(
+              core_based_statistical_area_id: cbsa.id,
+              metropolitan_statistical_area: msa.id
+            ), :count
+          ).by(1)
+      end
+    end
+  end
+
+  def build_zip_to_cbsa(cbsa: '20000')
+    {
+      '10000': [cbsa]
+    }
+  end
+
+  def build_cbsa_to_msa(mdiv: '10001')
+    {
+      "#{mdiv}": ['20000']
+    }
+  end
+
+  def build_msa(mdiv: nil)
+    {
+      '20000': {
+        cbsa: '20000',
+        mdiv: mdiv,
+        name: 'Test',
+        lsad: 'Test',
+        population_2014: 100,
+        population_2015: 100
+      }
+    }
+  end
+
+  def build_service(
+    zip_to_cbsa:,
+    cbsa_to_msa:,
+    msa:
+  )
+    described_class.new(
+      zip_to_cbsa: zip_to_cbsa,
+      cbsa_to_msa: cbsa_to_msa,
+      msa: msa
+    )
+  end
+
+  def build_normal_service
+    build_service(
+      zip_to_cbsa: build_zip_to_cbsa,
+      cbsa_to_msa: {},
+      msa: build_msa
+    )
+  end
+
+  def build_no_msa_service
+    build_service(
+      zip_to_cbsa: build_zip_to_cbsa,
+      cbsa_to_msa: {},
+      msa: {}
+    )
+  end
+
+  def build_alternate_cbsa_service
+    build_service(
+      zip_to_cbsa: build_zip_to_cbsa(cbsa: '10001'),
+      cbsa_to_msa: build_cbsa_to_msa,
+      msa: build_msa(mdiv: '10001')
+    )
+  end
+end
