e9f6656ed4077087c94eeddfbb799efb0e866702
Joseph Choe <joseph@josephchoe.com>
2018-02-13 10:45:44 -0800

    Add requires to base

--- population-growth-client/lib/population_growth_client.rb
+++ population-growth-client/lib/population_growth_client.rb
@@ -1,5 +1,7 @@
-require "population_growth_client/version"
-
-module PopulationGrowthClient
-  # Your code goes here...
-end
+require 'population_growth_client/version'
+require 'population_growth_client/client'
+require 'population_growth_client/response/error'
+require 'population_growth_client/response/parse_error'
+require 'population_growth_client/response/parse_json'
+require 'population_growth_client/response/parse_population'
+require 'population_growth_client/response/response'
--- population-growth-client/spec/population_growth_client_spec.rb
+++ population-growth-client/spec/population_growth_client_spec.rb
@@ -1,11 +1,7 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe PopulationGrowthClient do
-  it "has a version number" do
-    expect(PopulationGrowthClient::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'has a version number' do
+    expect(PopulationGrowthClient::VERSION).not_to be_empty
   end
 end
